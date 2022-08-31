dd25fc2ae9a1d2ed03039b62065dc483db1cfd90
Said <Said@10.0.0.176>
2022-08-30 18:28:00 -0600

    hi

---
+++ File_Handling/test.py
@@ -0,0 +1,16 @@
+'''
+Created on Aug. 30, 2022
+
+@author: Said
+'''
+import os
+'''
+f = open("testdoc.txt", "w")
+f.write(" hi there")
+f.close()
+f = open("testdoc.txt", "r")
+print(f.read())
+'''
+
+
+os.remove("newFile.txt")
